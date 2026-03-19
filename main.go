da7490ce71bf83d0bd814ddb536cd5ba427e141e
irshad Chanakya <35327176+mitrasoftware@users.noreply.github.com>
2026-03-19 19:33:30 +0530

    initial commit

--- main.go
+++ main.go
@@ -3,17 +3,31 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/mitrasoftware/cicdtest/models"
 )
 
 func main() {
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("NO .env file found")
+	}
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
 	http.HandleFunc("/getuser", getUser)
 	// http.HandleFunc("/createUser", createUser)
-	fmt.Println("Server started on port ", "8080")
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Server started on port ", port)
+	http.ListenAndServe(":"+port, nil)
 }
 
 func getUser(w http.ResponseWriter, r *http.Request) {
